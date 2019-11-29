57441078ccf5b5d2ff7e5e6b05643d168dd9306e
Pakeeza <32482263+Hotaru29@users.noreply.github.com>
2019-11-29 20:45:18 +0500

    Homework and assignments
    assignments

---
+++ class1/piaic/python1/c1.py
@@ -0,0 +1,16 @@
+student_name = "abc def"
+program = "PIAIC"
+fathers_name = "bbb ccc"
+age = 26
+score = 200
+
+info = """
+PIAIC Islamabad Batch 3
+Name : {student_name}
+Father's Name : {fathers_name}
+Age : {age}
+Score : {score}
+Program Name : {program}
+"""
+
+print(info)
